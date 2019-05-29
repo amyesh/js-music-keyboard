825fd9f086c40059c8e48d697c8430b2ea79a3f3
amyesh <amyesh08@gmail.com>
2019-05-28 22:07:15 -0700

    created helper method to play note

--- noise.js
+++ noise.js
@@ -1,21 +1,22 @@
 $(document).ready( function() {
   const keyNotes = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
 
+  const playNote = function(key) {
+    let note = `#${key}Audio`
+    if ($(note)[0].paused) {
+        $(note)[0].play();
+      } else {
+        $(note)[0].currentTime = 0;
+      }
+  }
+
   keyNotes.forEach( k => {
     $(`.${k}`).click(function() {
-      if ($(`#${k}Audio`)[0].paused) {
-        $(`#${k}Audio`)[0].play();
-      } else {
-        $(`#${k}Audio`)[0].currentTime = 0;
-      }
+      playNote(k);
     })
     $('body').keypress((event) => {
       if (event.key === `${k}`) {
-        if ($(`#${k}Audio`)[0].paused) {
-          $(`#${k}Audio`)[0].play();
-        } else {
-          $(`#${k}Audio`)[0].currentTime = 0;
-        }
+        playNote(k);
       }
     })
   })
